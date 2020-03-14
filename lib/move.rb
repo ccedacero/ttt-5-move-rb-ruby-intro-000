002c1a1dc6059fe38212b661390574d5bf9f77b3
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-14 18:43:39 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -27,3 +27,11 @@ Your `#move` method must take in three arguments, the board array, the index in 
 `#move` should also return the modified array with the updated index corresponding to the player's token. Don't create a new local variable for the board array, modify the one passed in as the argument and return it.
 
 Part of your `#move` method will mean updating the `board` `Array` passed into it.
+
+1. Our program should first welcome the player by outputting a friendly message to the terminal: "Welcome to Tic Tac Toe!".
+2. Next, establish the starting state of the game, i.e. the empty board. Create a new board by setting a variable `board` equal to instantiating a new array with 9 elements, each of which is a blank space, `" "`.  
+3. Now, ask the user for input by outputting "Where would you like to go?" to the terminal.
+4. We need to store the user's input. Use `gets.strip` to store their input to a variable, `input`.
+5. Now we want to convert the user's input to an array index using our `#input_to_index` method and store this as the variable `index`.
+6. Now we're ready to call our `#move` method. Do so with the arguments of the `board`, the `index` the user wants to access and the default player of `"X"`.
+7. Lastly, display the board by calling the `#display_board` method, passing in the necessary arguments required to run this method.
